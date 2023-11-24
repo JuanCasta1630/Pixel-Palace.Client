9b82b9b03173f9636d9808a327d65c255433258b
Nallis02 <nallive14@gmail.com>
2023-11-24 08:50:40 -0500

    se estan haciendo pruebas para el carrito de compras

--- src/app/game-details/[id]/page.tsx
+++ src/app/game-details/[id]/page.tsx
@@ -13,20 +13,25 @@ import { useRouter } from "next/navigation";
 import HeaderLayout from "@/app/components/Header";
 import { getProductById } from "@/app/servers/reques";
 import cookies from "js-cookie";
-
+import Link from "next/link";
+import { useCart } from "@/context/CarContext";
 
 function GameDetails() {
-
   const { Content } = Layout;
   const [data, setData] = useState<Game | null>(null);
   const [loading, setLoading] = useState(true);
   const pathname = usePathname();
-  const gameDetailId: any  = pathname.split("/").at(2);
+  const gameDetailId: any = pathname.split("/").at(2);
   const router = useRouter();
   const setProductId = (id: any) => {
     cookies.set("productId", id);
   };
-  setProductId(gameDetailId)
+  //@ts-ignore
+  const { state, addToCart } = useCart();
+  const handleAddToCart = () => {
+    addToCart(data);
+  };
+  setProductId(gameDetailId);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -39,7 +44,7 @@ function GameDetails() {
           ? await getProductById(gameDetailId)
           : await getGameDetails(gameDetailId);
         let gameDetailsData: any;
-  
+
         // Si es un ID alfanumérico, se espera que la respuesta tenga una propiedad 'data'
         if (isNumericId) {
           gameDetailsData = gameDetailsResponse?.data || {};
@@ -47,7 +52,7 @@ function GameDetails() {
           // Si es un ID numérico, se espera que la respuesta sea directamente el objeto de datos
           gameDetailsData = gameDetailsResponse || {};
         }
-        setData(gameDetailsData)
+        setData(gameDetailsData);
       } catch (error: any) {
         console.error(error);
       } finally {
@@ -62,13 +67,18 @@ function GameDetails() {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Layout className="w-full min-h-screen dark:bg-gray-700 bg-white">
-      <HeaderLayout />
+        <HeaderLayout />
         <Content className="p-4 w-full">
           <Card className="w-full mt-20 dark:bg-gray-200">
             <div className="container w-full flex flex-col md:flex-row items-start gap-8 px-4 md:px-6">
               <div className="flex-1">
                 <img
-                  src={data?.imagen ? data.imagen : data?.imageUrl || 'https://pixel-palace.netlify.app/logo.png'}
+                  src={
+                    data?.imagen
+                      ? data.imagen
+                      : data?.imageUrl ||
+                        "https://pixel-palace.netlify.app/logo.png"
+                  }
                   width="500"
                   height="500"
                   alt={data?.nombre}
@@ -80,10 +90,13 @@ function GameDetails() {
                   {data?.nombre}
                 </h1>
                 <p className="text-base text-zinc-500 dark:text-zinc-500">
-                  {data?.fecha_lanzamiento ? `Release Date: ${data.fecha_lanzamiento}` : null}
+                  {data?.fecha_lanzamiento
+                    ? `Release Date: ${data.fecha_lanzamiento}`
+                    : null}
                 </p>
                 <p className="text-base text-zinc-500 dark:text-zinc-500">
-                  Category: {data?.categoria ? data?.categoria : data?.categories}
+                  Category:{" "}
+                  {data?.categoria ? data?.categoria : data?.categories}
                 </p>
                 <div className="flex space-x-2">
                   <button className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 px-4 py-2 w-12 h-12 rounded-md border border-zinc-200 text-zinc-900 bg-white shadow-sm dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-50">
@@ -145,15 +158,23 @@ function GameDetails() {
                     $ {data?.precio ? data.precio : data?.price}
                   </p>
                   <div className="flex flex-col items-center space-y-2">
-                    <button className="button1 w-full text-white px-4 py-2 rounded" onClick={()=>router.push('/checkout')}>
+                    <button
+                      className="button1 w-full text-white px-4 py-2 rounded"
+                      onClick={() => router.push("/checkout")}
+                    >
                       Buy Now
                     </button>
-                    <button className="button1 w-full text-white px-4 py-2 rounded">
+                    <button  onClick={handleAddToCart} className="button1 w-full text-white px-4 py-2 rounded">
                       <span className="self-center">
                         <ShoppingCartOutlined className="font-semibold mr-2 text-md" />
                         Add to Cart
                       </span>
                     </button>
+                    <Link href="/cart">
+                      <button className="button1 w-full text-red px-4 py-2 rounded">
+                        View Cart
+                      </button>
+                    </Link>
                   </div>
                 </div>
               </div>
